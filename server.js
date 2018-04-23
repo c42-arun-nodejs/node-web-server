2d0b89f7e21fadd0251672524e6683ece3815f5b
Arunkumar Ramachandriya <arun@Arunkumars-MacBook-Pro.local>
2018-04-23 23:02:49 +0100

    Partial views

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
