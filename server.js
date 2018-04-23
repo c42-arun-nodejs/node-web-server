c50d7ad3dd15533be2599f578b580fa4d169c9a4
Arunkumar Ramachandriya <arun@Arunkumars-MacBook-Pro.local>
2018-04-23 23:07:08 +0100

    Partial helper function

--- server.js
+++ server.js
@@ -7,18 +7,20 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'Home page',
-        welcomeMessage: 'Welcome to my website!',
-        currentYear: new Date().getFullYear()
+        welcomeMessage: 'Welcome to my website!'
     });
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: 'About page',
-        currentYear: new Date().getFullYear()
+        pageTitle: 'About page'
     });
 });
 
